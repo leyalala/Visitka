0a7593a32d14ccf4eb8d223d3b9f9b56eaadcd17
leyalala <152689192+leyalala@users.noreply.github.com>
2024-06-19 19:31:08 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,25 @@
+
+
+function dark_mode(){
+    let is_dark = localStorage.getItem('dmode')
+    if (is_dark == 'true'){
+        is_dark=false
+    }else{
+        is_dark=true
+    }
+    localStorage.setItem('dmode', is_dark)
+    document.querySelector('.photo').classList.toggle('main-2', is_dark)
+    document.querySelectorAll('.annotaciya').forEach(element => {
+        element.classList.toggle('main-1',is_dark)
+    });
+    document.querySelector('body').classList.toggle('main-1', is_dark)
+}
+let button = document.querySelector('.modee')
+
+button.addEventListener('click', dark_mode)
+
+
+
+
+
+
